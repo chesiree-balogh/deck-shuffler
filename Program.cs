21b9f20681901b17f1c187ccee19d7f8aab3eedd
chesiree-balogh <chezarium04@gmail.com>
2020-02-12 17:27:12 -0500

    finished explorer

--- Program.cs
+++ Program.cs
@@ -7,11 +7,13 @@ namespace deck_shuffler
   {
     static void Main(string[] args)
     {
-      Console.WriteLine("Welcome to C#");
+      Console.WriteLine("Let's pick a random card!");
+
       //deck needs 52 unique cards
       //all cards should be represented as a string/name ex "ace of hearts"
       //there are four suits: clubs, diamonds, hearts, spades
       //there are 13 rank: ace, 2. 3. 4. 5. 6. 7. 8. 9. 10. jack. queen. king
+
 
       //create new deck
       var deck = new List<string>();
@@ -35,21 +37,56 @@ namespace deck_shuffler
           deck.Add(facename + " of " + suitname);
         }
       }
+
+      //have deck be shuffled 
+      Random rnd = new Random();
+      // for i from n - 1 down to 1 do:
+      for (int i = deck.Count - 1; i >= 1; i--)
+      {
+        //   j = random integer up to i
+        int j = rnd.Next(i);
+        //   swap deck[i] with deck[j]
+        var cardAtI = deck[i];
+        var cardAtJ = deck[j];
+
+        deck[i] = cardAtJ;
+        deck[j] = cardAtI;
+      }
       //this rights out the cards that have been assigned 
       for (int deckIndex = 0; deckIndex < deck.Count; deckIndex++)
       {
         var card = deck[deckIndex];
-        Console.WriteLine($"{deckIndex} : {card}");
+
+      }
+
+      //display top card
+      var topOfDeck = deck[0];
+      Console.WriteLine(topOfDeck);
+
+
+      //while loop
+      var z = 1;
+
+      while (true)
+      {
+
+
+
+        //give user option to see the next card or quit
+        Console.WriteLine("Want to see the next card? Y or N");
+        var choice = Console.ReadLine();
+        if (choice == "y")
+        {
+          Console.WriteLine(deck[z]);
+          z++;
+        }
+        else
+        {
+          break;
+        }
       }
 
 
-
-      //then have it pick a random card value?
-
-
-      //have deck be shuffled 
-      //once shuffled display top card
-      //give user option to see the next card or quit
 
     }
   }
